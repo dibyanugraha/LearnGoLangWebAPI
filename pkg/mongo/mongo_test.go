b2280521ee794982eba7ce0dd4d0569ea884304f
Ariestya Dibyanugraha <ariestya@gitsconsulting.com>
2018-12-21 14:47:20 +0700

    Init Test

---
+++ pkg/mongo/mongo_test.go
@@ -0,0 +1,54 @@
+package mongo_test
+
+import (
+	root "TestRestAPI/pkg"
+	"TestRestAPI/pkg/mongo"
+	"log"
+	"testing"
+)
+
+const (
+	mongoUrl           = "localhost:27707"
+	dbName             = "test_db"
+	userCollectionName = "user"
+)
+
+func Test_UserService(t *testing.T) {
+	t.Run("CreateUser", createUserShouldInsertUserIntoMongo)
+}
+
+func createUserShouldInsertUserIntoMongo(t *testing.T) {
+	// Arrange
+	session, err := mongo.NewSession(mongoUrl)
+	if err != nil {
+		log.Fatalf("Unable to connect to mongo: %s", err)
+	}
+	defer session.Close()
+	userService := mongo.NewUserService(session.Copy(), dbName, userCollectionName)
+
+	testUserName := "integration_test_user"
+	testPassword := "integration_test_password"
+
+	user := root.User{
+		Username: testUserName,
+		Password: testPassword}
+
+	//Act
+	err = userService.Create(&user)
+
+	// Assert
+	if err != nil {
+		t.Error("Unable to create user: `%s`", err)
+	}
+	var results []root.User
+	session.GetCollection(dbName, userCollectionName).Find(nil).All(&results)
+
+	count := len(results)
+	if count != 1 {
+		t.Error("Incorrect number of results. Expected `1`, got `%i`", count)
+	}
+
+	if results[0].Username != user.Username {
+		t.Error("Incorrect username. Expected `%s`, got `%s`", testUserName, results[0].Username)
+	}
+}
